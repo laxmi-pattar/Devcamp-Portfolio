9aa758b46a5c97ad37418749746400867c4e6a43
Laxmi Pattar <laxmipattar710@gmail.com>
2019-08-21 16:50:15 +0530

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :portfolios, except: [:show]
-  
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'about-me', to: "pages#about"
-  get 'contact', to: "pages#contact"
+	resources :portfolios, except: [:show]
 
-  resources :blogs
+	get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+	get 'about-me', to: "pages#about"
+	get 'contact', to: "pages#contact"
 
-  root to: 'pages#home'
+	resources :blogs do
+		member do
+			get :toggle_status
+		end
+	end
+
+	root to: 'pages#home'
 end
