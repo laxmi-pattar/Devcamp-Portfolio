4dfdbb926cdb1cac5cfaed434508830ffea5b922
Laxmi Pattar <laxmipattar710@gmail.com>
2019-08-22 16:41:20 +0530

    Integrated technology model

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,32 +11,40 @@ puts "3 topics are created"
 		topic_id: Topic.last.id)
 end
 
-	puts "10 blogs are created"
+puts "10 blogs are created"
 
-	5.times do |skill|
-		Skill.create!(
-			title: "Rails#{skill}",
-			percent_utilized: 10 * skill)
-	end
+5.times do |skill|
+	Skill.create!(
+		title: "Rails#{skill}",
+		percent_utilized: 10 * skill)
+end
 
-	puts "5 skills created"
+puts "5 skills created"
 
-	8.times do |portfolio_item|
-		Portfolio.create!(
-			title: "Portfolio_item#{portfolio_item}",
-			subtitle: "Ruby on Rails",
-			body: "welcome to portfolio#{portfolio_item}",
-			main_image: "https://via.placeholder.com/600x400",
-			thumb_image: "https://via.placeholder.com/350x150")
-	end
+8.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio_item#{portfolio_item}",
+		subtitle: "Ruby on Rails",
+		body: "welcome to portfolio#{portfolio_item}",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/350x150")
+end
 
-		1.times do |portfolio_item|
-		Portfolio.create!(
-			title: "Portfolio_item#{portfolio_item}",
-			subtitle: "Angular",
-			body: "welcome to portfolio#{portfolio_item}",
-			main_image: "https://via.placeholder.com/600x400",
-			thumb_image: "https://via.placeholder.com/350x150")
-	end
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio_item#{portfolio_item}",
+		subtitle: "Angular",
+		body: "welcome to portfolio#{portfolio_item}",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/350x150")
+end
 
-	puts "9 portfolios created"
+puts "9 portfolios created"
+
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology#{technology}",
+		portfolio_id: Portfolio.last.id)
+end
+
+puts "3 technologies created"
