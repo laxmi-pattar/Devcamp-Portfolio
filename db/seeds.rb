3bc2bcb1d36ffcdd64ddaaaa0bc0c7288f9fd1f5
Laxmi Pattar <laxmipattar710@gmail.com>
2019-08-22 12:25:50 +0530

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+	Topic.create!(title: "Topic#{topic}")
+end
+
+puts "3 topics are created"
+
 10.times do |blog|
 	Blog.create!(
 		title: "My blog post#{blog}",
-		body: "welcome to the blog#{blog}")
+		body: "welcome to the blog#{blog}",
+		topic_id: Topic.last.id)
 end
 
 	puts "10 blogs are created"
@@ -14,13 +21,22 @@ end
 
 	puts "5 skills created"
 
-	9.times do |portfolio_item|
+	8.times do |portfolio_item|
 		Portfolio.create!(
 			title: "Portfolio_item#{portfolio_item}",
-			subtitle: "subtitle#{portfolio_item}",
+			subtitle: "Ruby on Rails",
 			body: "welcome to portfolio#{portfolio_item}",
 			main_image: "https://via.placeholder.com/600x400",
 			thumb_image: "https://via.placeholder.com/350x150")
 	end
 
-	puts "9 portfolioss created"
+		1.times do |portfolio_item|
+		Portfolio.create!(
+			title: "Portfolio_item#{portfolio_item}",
+			subtitle: "Angular",
+			body: "welcome to portfolio#{portfolio_item}",
+			main_image: "https://via.placeholder.com/600x400",
+			thumb_image: "https://via.placeholder.com/350x150")
+	end
+
+	puts "9 portfolios created"
