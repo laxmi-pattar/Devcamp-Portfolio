51f6906a237707d05276a301c6c136f68feb4c3a
Laxmi Pattar <laxmipattar710@gmail.com>
2019-09-03 16:38:12 +0530

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,17 @@ module ApplicationHelper
 		end 
 	end
 
-	#def nav_helper(style, tag_type)
+	def alerts
+		alert = (flash[:notice] || flash[:error] || flash[:alert])
+		if alert
+     alert_generator alert 
+		end
+	end
+	def alert_generator msg
+  js add_gritter(msg, title: "My Portfolio", sticky: false) 
+	end
+
+#def nav_helper(style, tag_type)
 #nav_links = <<NAV
 #<#{tag_type}><a href="#{root_path}", class: "#{style} #{active? root_path}">Home</a></#{tag_type}>
 #<#{tag_type}><a href="#{about_me_path}", class: "#{style} #{active? about_me_path}">About Me</a></#{tag_type}>
