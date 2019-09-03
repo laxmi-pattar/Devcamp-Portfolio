c6d31ef789f58d052fdcca2a4696b07614af89dd
Laxmi Pattar <laxmipattar710@gmail.com>
2019-09-03 12:47:42 +0530

    Updated file upload forms and refactored placeholder functionality into view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -65,7 +65,7 @@ class PortfoliosController < ApplicationController
  end
 
  def portfolio_params
- 	params.require(:portfolio).permit(:title, :subtitle, :body,
+ 	params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image,
  		technologies_attributes: [:name])
  end
 end
